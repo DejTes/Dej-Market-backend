ef4ce9c650b403de408ba31d1a6057ba184a124d
dejtes <dejenaref@gmail.com>
2023-04-04 17:50:03 -0400

    controllers and models added

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - add `require('dotenv').config();` in app.js
 - add .env to .gitignore
 - npm i mongoose
+- npm i validator   More on Validation: [validator](https://www.npmjs.com/package/validator)
+- npm i http-status-codes
+
+ - npm i morgan  [morgan](https://www.npmjs.com/package/morgan)
--- app.js
+++ app.js
@@ -2,15 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+
+const morgan = require('morgan')
 //database
 const connectDB = require('./db/connect')
 
+
+//ROUTERS
+const authRouter = require('./routes/authRoutes')
+
+
+
+
 //middleware
 app.use(express.json())
+app.use(morgan('tiny'))
+
 
 app.get('/', (req, res) => {
     res.send("dej-market started")
 })
+
+
+app.use('/api/market/auth', authRouter)
+
+
 
 const port = process.env.PORT || 8000;
 const start = async () => {
